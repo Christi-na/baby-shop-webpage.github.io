baa4ba314163dd5858dd0a725a04b7f83ee8d17e
Christi-na <76699605+Christi-na@users.noreply.github.com>
2022-09-09 12:23:21 +0300

    Responsive features

--- scripts/code.js
+++ scripts/code.js
@@ -1,18 +1,19 @@
 // Toggle Menu
 let navBar = document.getElementById('nav-bar');
 let pageBody = document.getElementsByTagName('body');
-let showIcon = document.getElementById('mobile-menu_show');
+//let showIcon = document.getElementById('mobile-menu_show');
 let hideIcon = document.getElementById('mobile-menu_hide');
 
 
 function showMenu(){
     navBar.style.left = '0';
     pageBody[0].style.overflow = 'hidden';
-    showIcon.style.display = 'none';
+    //showIcon.style.display = 'none';
     hideIcon.style.display = 'block';
 }
 function hideMenu(){
     navBar.style.left = '-1000px';
-    showIcon.style.display = 'block';
+    //showIcon.style.display = 'block';
     hideIcon.style.display = 'none';
+    pageBody[0].style.overflow = 'scroll';
 }
